066d61cc1add36301a304a02ae8f636a71f10a9d
小荣 <yanxiangrong_2@outlook.com>
2024-03-11 18:42:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+set -xve
+
+REPO_URL="https://github.com/coolsnowwolf/lede"
+REPO_BRANCH="master"
+CONFIG_FILE="configs/x86_64.config"
+DIY_SCRIPT="diy-script.sh"
+CLASH_KERNEL="amd64"
+FIRMWARE_TAG="X86_64"
+TZ="Asia/Shanghai"
+
 echo "警告⚠"
 echo "分配的服务器性能有限，若选择的插件过多，务必注意CPU性能！"
 echo -e "已知CPU型号(降序): 7763，8370C，8272CL，8171M，E5-2673\n"
@@ -14,7 +25,7 @@ echo "硬盘数量: $(ls /dev/sd* | grep -v [1-9] | wc -l)" && df -hT
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo timedatectl set-timezone Asia/Shanghai
+sudo timedatectl set-timezone $TZ
 
 sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
@@ -24,30 +35,30 @@ libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgcon
 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo \
 uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
 
-mkdir workspace
+git clone https://github.com/yanxiangrong/OpenWrt.git workspace
 cd workspace
-df -hT .
+GITHUB_WORKSPACE=$(pwd)
+chmod +x $GITHUB_WORKSPACE/scripts/*.sh
+chmod +x $DIY_SCRIPT
+df -hT $GITHUB_WORKSPACE
 
-git clone https://github.com/yanxiangrong/OpenWrt.git
-git clone https://github.com/coolsnowwolf/lede
-
-chmod +x OpenWrt/scripts/*.sh
-chmod +x OpenWrt/diy-script.sh
-cp OpenWrt/configs/x86_64.config lede/.config
-
-cd lede
-make defconfig
+git clone $REPO_URL -b $REPO_BRANCH openwrt
+cd openwrt
+OPENWRT_PATH=$(pwd)
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-../OpenWrt/diy-script.sh
-../OpenWrt/scripts/preset-clash-core.sh amd64
-../OpenWrt/scripts/preset-terminal-tools.sh
-../OpenWrt/scripts/preset-adguard-core.sh amd64
+cp $GITHUB_WORKSPACE/$CONFIG_FILE $OPENWRT_PATH/.config
+
+$GITHUB_WORKSPACE/$DIY_SCRIPT
+$GITHUB_WORKSPACE/scripts/preset-clash-core.sh $CLASH_KERNEL
+$GITHUB_WORKSPACE/scripts/preset-terminal-tools.sh
+$GITHUB_WORKSPACE/scripts/preset-adguard-core.sh $CLASH_KERNEL
+make defconfig
+make download -j8
 
 mkdir -p files/etc/uci-defaults
-cp ../OpenWrt/scripts/init-settings.sh files/etc/uci-defaults/99-init-settings
+cp $GITHUB_WORKSPACE/scripts/init-settings.sh files/etc/uci-defaults/99-init-settings
 
-make download -j32
-make -j16
+make -j8
